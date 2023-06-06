3a058f88ba5eed7af2e858205b799ece0fcf89bf
Afranio Viana <18465@bemol.local>
2023-06-06 11:36:52 -0400

    Array push e pop[

--- arrays.js
+++ arrays.js
@@ -1,5 +1,19 @@
-const notas=[4,5,10,2];
+/*Mesmo sendo uma constante, um array pode ser modificado
+a partir de alguns métodos específicos */
+const notas=[4,5,10];
 
+
+/*O "push" serve para inserir um elemento no final do array */
+notas.push(10);
+
+/*O "length" recupera a quantidade de valores dentro de 
+um array */
 let media=(notas[0]+notas[1]+notas[2]+notas[3])/notas.length;
 
-console.log(media);
+console.log(notas.length);
+
+
+/*O "pop" retira o último elemento do array */
+notas.pop();
+
+console.log(notas.length);
