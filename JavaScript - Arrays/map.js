ebc3358338c8e995d187072817fa2baeea31d0a6
Afranio Viana <18465@bemol.local>
2023-06-09 08:45:52 -0400

    Utilizando o map

---
+++ JavaScript - Arrays/map.js
@@ -0,0 +1,21 @@
+const notas=[10, 9.5, 8, 7, 4];
+
+
+/*O map serve para percorrer todo um array, mas não substitui
+o forEach */
+const notasAtualizadas=notas.map((nota)=>{
+    return nota+1 >=10 ? 10 : nota+1;
+});
+
+console.log(notasAtualizadas);
+
+
+/*Mesmo código, mas utilizando o forEach */
+const notasAtualizadas2=[];
+notas.forEach(nota=>{
+    notasAtualizadas2.push(nota+1>=10?10:nota+1);
+});
+
+console.log(notasAtualizadas2);
+
+console.log(notas);
