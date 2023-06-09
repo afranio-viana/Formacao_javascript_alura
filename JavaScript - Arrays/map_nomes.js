0df136e1b8d39d77d41a1c27a7edf9be21a9e0dd
Afranio Viana <18465@bemol.local>
2023-06-09 09:00:32 -0400

    Utilizando o map para padronizar as strings de um array

---
+++ JavaScript - Arrays/map_nomes.js
@@ -0,0 +1,7 @@
+const nomes = ['Afrânio', 'esquerdO', 'vIana'];
+
+/*Aqui o map foi utilizado para padronizar
+as strings de dentro de um array*/
+const nomesPadronizados=nomes.map((nome)=>nome.toUpperCase());
+
+console.log(nomesPadronizados);
