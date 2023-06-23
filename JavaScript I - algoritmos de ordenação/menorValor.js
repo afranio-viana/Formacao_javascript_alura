79503ebcf921bbd0e0ce8b26d7ba016fce87b8fa
Afranio Viana <18465@bemol.local>
2023-06-23 09:56:03 -0400

    Implementando o Selection Sort

---
+++ JavaScript I - algoritmos de ordenação/menorValor.js
@@ -0,0 +1,13 @@
+const livros=require('./listaLivros');
+
+function menorValor(arrProdutos,posicaoIncial){
+    let maisBarato=posicaoIncial;
+    for(let atual =posicaoIncial; atual<arrProdutos.length;atual++){
+        if(arrProdutos[atual].preco<arrProdutos[maisBarato].preco){
+            maisBarato=atual;
+        }
+    }
+    return maisBarato;
+}
+
+module.exports=menorValor;
